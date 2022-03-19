675d02fd51e3e377ab1b65de91aa1494c55d69e1
khanh <khanh@khanhs-Air.lan>
2022-03-19 15:21:00 +0700

    done

--- src/components/sidebar/sidebar_playlist/SidebarPlaylist.tsx
+++ src/components/sidebar/sidebar_playlist/SidebarPlaylist.tsx
@@ -3,6 +3,8 @@ import {NavLink} from "react-router-dom";
 import style from "./SidebarPlaylist.module.css"
 import ClearIcon from "@mui/icons-material/Clear";
 import PopUpDel from "../../pop_up/delete_cf/PopUpDel";
+import {getImageUrl, getListUri, getPlaylistID, getPlaylistName} from "../../../redux/actions/actions";
+import {useDispatch} from "react-redux";
 
 interface ISidebarPlaylist {
     name: string;
@@ -16,6 +18,8 @@ interface ISidebarPlaylist {
 
 export default function SidebarPlaylist(props:ISidebarPlaylist){
     const [isOpen,setIsOpen] = useState(false)
+    const dispatch = useDispatch()
+
     // const a = state.count
     const handleClose = (data: any) => {
         setIsOpen(data);
@@ -26,9 +30,18 @@ export default function SidebarPlaylist(props:ISidebarPlaylist){
 
     }
 
+    const getInfoHandle = ( name: string, id: string, uri: string) => {
+        dispatch(getPlaylistName(name))
+        dispatch(getPlaylistID(id))
+        dispatch(getListUri(uri))
+    }
+
     return (
         <div className={style.container}>
             <NavLink
+                onClick={() => {
+                    getInfoHandle( props?.name, props?.id, props?.uri)
+                }}
                      style={{ textDecoration: "none" }} to={`/playlist/${props.id}`}>
                 <div className={style.SideBarPlaylist}>
                     <PopUpDel open={isOpen} playlistId={props.id} onClose={handleClose} index={props.index} currName={props.name}/>
